53dba56ae4a5f4f595eb5350b9652e2d9aa8632b
Reishel-pere15 <reishelp15@gmail.com>
2026-04-17 12:02:11 -0400

    Revision error archivo

--- TallerIUJO01/TallerIUJO01/Program.cs
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -7,6 +7,7 @@
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
+using System.IO;
 
 namespace TallerIUJO01
 {
@@ -17,15 +18,23 @@ namespace TallerIUJO01
 			Console.WriteLine("-----TALLER 01------");
 			
 			// 1. El dato del usuario
-			string registroUsuario = "   ID_777;   EVALUACION;    95";
+			string registroUsuario = "   ID_777;  juanperez;  EVALUACION;    95";
 			Console.WriteLine(registroUsuario);
 			string registroLimpio = registroUsuario.Trim();
 			Console.WriteLine(registroLimpio);
 			string[] partes = registroLimpio.Split(';');
 			string id = partes[0].Trim();
 			string nombre = partes[1].Trim();
-			string nota = partes[2].Trim();
+			string tarea = partes[2].Trim();
+			string nota = partes[3].Trim();
 			Console.WriteLine(string.Format("el id es: {0} del usuario {1} con la nota {2}",id,nombre,nota));
+			//Flujo de Archivos
+			string rutaraiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"DatosIUJO");
+			string archivotexto =Path.Combine(rutaraiz,"notas.txt");
+			
+			Console.WriteLine(archivotexto);
+			using( StreamWriter sw = new StreamWriter(archivotexto,true));
+			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
